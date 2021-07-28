ab70343eff41e5181dffa36bf01ac8882c66f489
Henry Lin <linhenry2004@gmail.com>
2021-07-29 01:46:10 +0800

    Classes and Objects

---
+++ CH08/Classes_and_Objects.java
@@ -0,0 +1,10 @@
+public class Classes_and_Objects {
+    int x = 5;
+
+    public static void main(String args[]) {
+        Classes_and_Objects myObj1 = new Classes_and_Objects();
+        Classes_and_Objects myObj2 = new Classes_and_Objects();
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
+}
